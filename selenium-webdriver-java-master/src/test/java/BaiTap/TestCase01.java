3a7b04e8ca10b214f7f4cb008aa606f19023e93f
KayCCCCCC <nguyenngothanhnha2003kg@gmail.com>
2023-10-25 17:32:24 +0700

    add

---
+++ selenium-webdriver-java-master/src/test/java/BaiTap/TestCase01.java
@@ -0,0 +1,63 @@
+package BaiTap;
+
+import driver.driverFactory;
+import org.openqa.selenium.*;
+import org.openqa.selenium.io.FileHandler;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.time.Duration;
+
+/*
+
+Test Steps
+
+Step 1. Go to http://live.techpanda.org/
+
+Step 2. Verify Title of the page
+
+Step 3. Click on -> MOBILE -> menu
+
+Step 4. In the list of all mobile , select SORT BY -> dropdown as name
+
+Step 5. Verify all products are sorted by name
+
+*/
+@Test
+public class TestCase01 {
+    public static void TestCase01() {
+        WebDriver driver = driverFactory.getChromeDriver();
+        try {
+            // 1. Open the target page
+            driver.get("http://live.techpanda.org/");
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+            // 2. Verify Title of the page
+            WebElement titleElem = driver.findElement(By.xpath("//h2[1]"));
+            String actualTitle = titleElem.getText();
+            String expectTitle = "THIS IS DEMO SITE FOR   ";
+
+            Assert.assertEquals(expectTitle, actualTitle, "Title does not match the expected title");
+
+            // 3. Click on -> MOBILE -> menu
+            WebElement mobileMenu = driver.findElement(By.xpath("//a[normalize-space()='Mobile']"));
+            mobileMenu.click();
+
+            // 4. In the list of all mobile, select SORT BY -> dropdown as name
+            WebElement dropdownElem = driver.findElement(By.xpath("//body/div[@class='wrapper']/div[@class='page']/div[@class='main-container col3-layout']/div[@class='main']/div[@class='col-wrapper']/div[@class='col-main']/div[@class='category-products']/div[@class='toolbar']/div[@class='sorter']/div[@class='sort-by']/select[1]"));
+            Select optionElem = new Select(dropdownElem);
+            optionElem.selectByVisibleText("Name");
+            // 5. Verify all products are sorted by name
+            TakesScreenshot screenshot = ((TakesScreenshot) driver);
+            File srcFile = screenshot.getScreenshotAs(OutputType.FILE);
+            FileHandler.copy(srcFile, new File("D:\\CN5\\SWT301\\selenium-webdriver-java-master\\selenium-webdriver-java-master\\src\\test\\java\\BaiTap\\textcase01.png"));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        //7. Quit browser session
+        driver.quit();
+    }
+}
---
+++ selenium-webdriver-java-master/src/test/java/BaiTap/TestCase02.java
@@ -0,0 +1,70 @@
+package BaiTap;
+
+import driver.driverFactory;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.time.Duration;
+//        1. Goto http://live.techpanda.org/
+//
+//        2. Click on �MOBILE� menu
+//
+//        3. In the list of all mobile , read the cost of Sony Xperia mobile (which is $100)
+//
+//        4. Click on Sony Xperia mobile
+//
+//        5. Read the Sony Xperia mobile from detail page.
+//
+//        6. Compare Product value in list and details page should be equal ($100).
+@Test
+public class TestCase02 {
+
+    public static void TestCase02() {
+        // Initialize the WebDriver
+        WebDriver driver = new ChromeDriver();
+
+        try {
+            // 1. Go to http://live.techpanda.org/
+            driver.get("http://live.techpanda.org/");
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+
+            // 2. Click on "MOBILE" menu
+            WebElement mobileMenu = driver.findElement(By.xpath("//a[normalize-space()='Mobile']"));
+            mobileMenu.click();
+
+            // 3. Read the cost of Sony Xperia mobile (which is $100)
+            WebElement sonyXperiaPriceElement = driver.findElement(By.xpath("//span[contains(text(),'$100.00')]"));
+            String sonyXperiaPrice = sonyXperiaPriceElement.getText();
+
+            // 4. Click on Sony Xperia mobile
+            WebElement sonyXperiaLink = driver.findElement(By.xpath("//img[@id='product-collection-image-1']"));
+            sonyXperiaLink.click();
+
+            // 5. Read the Sony Xperia mobile from the detail page
+            WebElement sonyXperiaDetailPriceElement = driver.findElement(By.xpath("//span[@class='price']"));
+            String sonyXperiaDetailPrice = sonyXperiaDetailPriceElement.getText();
+
+            // 6. Compare Product value in list and details page should be equal ($100)
+            if (sonyXperiaPrice.equals(sonyXperiaDetailPrice)) {
+                System.out.println("Prices match: " + sonyXperiaPrice);
+            } else {
+                System.out.println("Prices do not match");
+            }
+            TakesScreenshot screenshot = ((TakesScreenshot) driver);
+            File srcFile = screenshot.getScreenshotAs(OutputType.FILE);
+            FileHandler.copy(srcFile, new File("D:\\CN5\\SWT301\\selenium-webdriver-java-master\\selenium-webdriver-java-master\\src\\test\\java\\BaiTap\\textcase02.png"));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // Quit the browser
+            driver.quit();
+        }
+    }
+}
